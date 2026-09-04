b6041ee661ae1c290829efe2a254e844a0905c47
SHREYAS-606 <devadigashreyas768@gmail.com>
2026-09-04 21:22:12 +0530

    Add Codeforces solution

---
+++ 1858a.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long a, b, c;
+        cin >> a >> b >> c;
+
+        if (c % 2 == 0) {
+            if (a > b)
+                cout << "First\n";
+            else
+                cout << "Second\n";
+        }
+        else {
+            if (a >= b)
+                cout << "First\n";
+            else
+                cout << "Second\n";
+        }
+    }
+
+    return 0;
+}
